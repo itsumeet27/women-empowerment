985df5d56696335dd9f07bd58e038e6076067ea5
Sumeet Sharma <sksksharma0@gmail.com>
2019-09-17 00:27:35 +0530

    Adding contents for NGO

--- ngo/index.php
+++ ngo/index.php
@@ -1,17 +1,37 @@
 <?php include 'includes/header.php';?>
 
-  <!-- Start your project here-->
-  <div style="height: 100vh">
-    <div class="flex-center flex-column">
-
-      <h1 class="text-hide animated fadeIn mb-4"
-        style="background-image: url('https://mdbootstrap.com/img/logo/mdb-transparent-250px.png'); width: 250px; height: 90px;">
-        NGO</h1>
-      <h5 class="animated fadeIn mb-3">Thank you for using our product. We're glad you're with us.</h5>
-
-      <p class="animated fadeIn text-muted">MDB Team</p>
-    </div>
-  </div>
-  <!-- Start your project here-->
+  <h2 class="h2-responsive text-center p-3">Non-Government Organizations (NGO)</h2>
+  <p class="p-2 text-justify" id="content">
+    Non- governmental organizations (NGOs) can be defined as, “legally constituted corporations created by natural or
+    legal people that operate independently from any form of government. The term originated from the United Nations and
+    normally refers to the organizations that are not a part of the government and are not conventional for profit
+    businesses. In the cases in which NGOs are totally or partially funded by the governments, the NGO maintains its
+    non-governmental status by excluding government representatives from membership in the organization.
+  </p>
+  <h4 class="h4-responsive p-2">List of top five NGOs in India</h4>
+  <p class="p-2 text-justify" id="content">
+   <b> 1. SAMMAAN FOUNDATION (established January 25, 2007):</b> Originally established to link the poor to the mainstream
+    through education, training and financial support, the current project of this NGO involves the rickshaw pullers to
+    help them earn a better livelihood. This NGO also has notable contribution in areas like children education, health
+    services and welfare of women.
+  </p>
+  <p class="p-2 text-justify" id="content">
+    <b>2. GOONJ:</b> A recipient of the “NGO of the Year” award in 2007 at the India NGO Awards, this NGO aims at solving the
+    clothing problems of the downtrodden. Goonj also provides relief during Rahat floods in West Bengal, Assam and Bihar.
+  </p>
+  <p class="p-2 text-justify" id="content">
+    <b>3. AKSHAYA TRUST:</b> The sole aim of this NGO is to restore human dignity. Operating in Madurai, this NGO offers
+    rehabilitation, healthy food and care to the street destitute.
+  </p>
+  <p class="p-2 text-justify" id="content">
+    <b>4. SMILE FOUNDATION:</b> The main aim of this NGO is the rehabilitation of the underprivileged by providing them education
+    and healthcare services, thereby converting them into productive assets. It is run by a group of corporate
+    professionals.
+  </p>
+  <p class="p-2 text-justify" id="content">
+    <b>5. UDAAN WELFARE FOUNDATION:</b> The main aim of this NGO is to help the destitute, the main area of stress being women,
+    children and senior citizens and also environmental welfare. One of their main projects is a cancer chemotherapy
+    center.
+  </p>
 
 <?php include 'includes/footer.php';?>  
