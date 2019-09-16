b8b12f389a4e6a0491868ae96b3c2b4580adf00c
Sumeet Sharma <sksksharma0@gmail.com>
2019-09-16 23:02:53 +0530

    Adding contents for website

--- index.php
+++ index.php
@@ -1,17 +1,80 @@
 <?php include 'includes/header.php';?>
 
-  <!-- Start your project here-->
-  <div style="height: 100vh">
-    <div class="flex-center flex-column">
+  <h2 class="h2-responsive text-center p-3">Welcome to Ministry of Women And Child Development</h2>
+  <p class="p-2 text-center" id="head-content">
+    "The Ministry of Women and Child Development, a branch of the Government of India, is the apex body for formulation
+    and
+    administration of the rules and regulations and laws relating to women and child development in India."
+  </p>
+  <div class="container-fluid">
+    <div class="row p-3">
+      <div class="col-lg-6 col-md-6 col-sm-12 col-xs-12">
+        <div class="card">
 
-      <h1 class="text-hide animated fadeIn mb-4"
-        style="background-image: url('https://mdbootstrap.com/img/logo/mdb-transparent-250px.png'); width: 250px; height: 90px;">
-        Women Empowerment</h1>
-      <h5 class="animated fadeIn mb-3">Thank you for using our product. We're glad you're with us.</h5>
+          <!-- Card image -->
+          <div class="view overlay">
+            <img class="card-img-top" src="img/women-empowerment-1.jpg" alt="STEP">
+            <a>
+              <div class="mask rgba-white-slight"></div>
+            </a>
+          </div>
 
-      <p class="animated fadeIn text-muted">MDB Team</p>
+          <!-- Card content -->
+          <div class="card-body elegant-color white-text rounded-bottom">
+
+            <!-- Social shares button -->
+            <a class="activator waves-effect mr-4"><i class="fas fa-share-alt white-text"></i></a>
+            <!-- Title -->
+            <h4 class="card-title">STEP</h4>
+            <hr class="hr-light">
+            <!-- Text -->
+            <p class="card-text white-text text-justify mb-4">The Ministry has been administering ‘Support to Training and Employment
+              Programme for Women (STEP) Scheme’ since 1986-87 as a ‘Central Sector Scheme’. The STEP Scheme aims to
+              provide
+              skills that give employability to women and to provide competencies and skill that enable women to become
+              self-employed/entrepreneurs. </p>
+            <!-- Link -->
+            <a href="user/index.php" class="btn btn-md btn-white">Read More</a>
+
+          </div>
+
+        </div>
+        <!-- Card Dark -->
+      </div>
+      <div class="col-lg-6 col-md-6 col-sm-12 col-xs-12">
+        <div class="card">
+
+          <!-- Card image -->
+          <div class="view overlay">
+            <img class="card-img-top" src="img/ngo.jpg" alt="NGO">
+            <a>
+              <div class="mask rgba-white-slight"></div>
+            </a>
+          </div>
+
+          <!-- Card content -->
+          <div class="card-body elegant-color white-text rounded-bottom">
+
+            <!-- Social shares button -->
+            <a class="activator waves-effect mr-4"><i class="fas fa-share-alt white-text"></i></a>
+            <!-- Title -->
+            <h4 class="card-title">NGO</h4>
+            <hr class="hr-light">
+            <!-- Text -->
+            <p class="card-text white-text text-justify mb-4">A non-governmental organization (NGO) is a non-profit, citizen-based
+              group
+              that functions independently of government. As non-profit organizations, NGOs rely on a variety of sources
+              for funding projects, operations, salaries and other overhead costs. NGO in our portal can register and add
+              courses for training programme for women</p>
+            <!-- Link -->
+            <a href="ngo/index.php" class="btn btn-md btn-white">Read More</a>
+
+          </div>
+
+        </div>
+        <!-- Card Dark -->
+      </div>
     </div>
   </div>
-  <!-- Start your project here-->
 
 <?php include 'includes/footer.php';?>
