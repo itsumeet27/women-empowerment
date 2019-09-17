d8a7ed7bc5a9398732b43ccdcf9ca5bde8195195
Sumeet Sharma <sksksharma0@gmail.com>
2019-09-17 20:40:02 +0530

    Adding contents for admin

--- admin/index.php
+++ admin/index.php
@@ -1,17 +1,5 @@
 <?php include 'includes/header.php';?>
 
-  <!-- Start your project here-->
-  <div style="height: 100vh">
-    <div class="flex-center flex-column">
-
-      <h1 class="text-hide animated fadeIn mb-4"
-        style="background-image: url('https://mdbootstrap.com/img/logo/mdb-transparent-250px.png'); width: 250px; height: 90px;">
-        Admin</h1>
-      <h5 class="animated fadeIn mb-3">Thank you for using our product. We're glad you're with us.</h5>
-
-      <p class="animated fadeIn text-muted">MDB Team</p>
-    </div>
-  </div>
-  <!-- Start your project here-->
+  <h2 class="h2-responsive text-center p-2">List of women(s) applied for training</h2>
 
 <?php include 'includes/footer.php';?>  
---
+++ admin/users.php
@@ -0,0 +1,5 @@
+<?php include 'includes/header.php';?>
+
+<h2 class="h2-responsive text-center p-2">List of registered women(s)</h2>
+
+<?php include 'includes/footer.php';?>
