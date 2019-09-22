8392af184ad40bbc2d91cbd56d6ded66254858f4
Sumeet Sharma <sksksharma0@gmail.com>
2019-09-22 14:18:24 +0530

    Apply for course

--- user/ngo.php
+++ user/ngo.php
@@ -1,4 +1,5 @@
 <?php 
+    session_start();
     include 'includes/header.php';
     include '../core/init.php';
 ?>
@@ -48,7 +49,15 @@
                         <label>Provided by: </label> <?=$courses['ngo_name'];?>
                     </div>
                     <div class="float-right">
-                        <a href="courses.php?apply=<?=$courses['id'];?>" id="apply" name="apply" class="btn btn-default btn-md">Apply Now</a>
+                        <?php 
+                            // Check if user is logged in
+                            if(!isset($_SESSION['email'])){
+                              echo "<a href='login.php' id='apply' name='apply' class='btn btn-default btn-md'>Apply Now</a>";
+                            }else{
+                        ?>
+
+                        <a href="ngo.php?apply=<?=$courses['id'];?>" id="apply" name="apply" class="btn btn-default btn-md">Apply Now</a>
+                    <?php } ?>
                     </div>                    
                 </div>
             </div>
@@ -57,5 +66,54 @@
         <?php endwhile; ?>
     </div>
 </div>
+
+<?php
+    // Select the user based on logged in email
+    $email = $_SESSION['email'];
+    $sqlcus = "SELECT * FROM step WHERE email = '$email'";
+    $result = $db->query($sqlcus);
+    while ($row_pro = mysqli_fetch_array($result)) {
+        $step_id = $row_pro['id'];
+        $step_name = $row_pro['fullname'];
+        $step_email = $row_pro['email'];
+        $step_address = $row_pro['address'];
+        $step_city = $row_pro['city'];
+        $step_state = $row_pro['state'];
+        $step_zipcode = $row_pro['zipcode'];
+        $step_phone = $row_pro['phone'];
+    }
+?>
+
+<?php
+    if(isset($_GET['apply'])){
+        $id = sanitize((int)$_GET['apply']);
+        $sqlcourse = "SELECT * FROM courses WHERE deleted = 0 AND id = '$id'";
+        $courses = $db->query($sqlcourse);
+        while ($course = mysqli_fetch_assoc($courses)) {
+            $course_name = $course['course_name'];
+        }
+        $sql = "SELECT * FROM applications WHERE course_id = '$id'";
+        $applications = $db->query($sql);
+        while ($application = mysqli_fetch_assoc($applications)) {
+            $step_user_id = $application['step_id'];
+            $course_user_id = $application['course_id'];
+        }
+        $insertSql = "INSERT INTO applications (`step_id`,`course_id`,`applied`) VALUES ('$step_id','$id', 1)";
+        $insert = $db->query($insertSql);
+        if($insert){
+        ?>
+            <script type="text/javascript">
+                alert('Successfully applied for the course!');
+            </script>
+        <?php
+        }else{
+            ?>
+            <script type="text/javascript">
+                alert('Sorry your application could not be added!');
+            </script>
+        <?php
+        }
+    }
+?>
 
 <?php include 'includes/footer.php';?>
