a73ca1ff2ac80b08b9edf730005504d55db64031
Sumeet Sharma <sksksharma0@gmail.com>
2019-09-25 13:26:44 +0530

    Changes in config for Heroku App

--- core/init.php
+++ core/init.php
@@ -1,12 +1,14 @@
 <?php
 
-$db = mysqli_connect('localhost', 'root', '', 'women_empowerment');
+// $db = mysqli_connect('localhost', 'root', '', 'women_empowerment');
+$db = mysqli_connect('us-cdbr-iron-east-02.cleardb.net', 'b648b7587f8192', 'b66f141a', 'heroku_b342f2365cab7e0');
 if(mysqli_connect_errno()){
 	echo "Database connection failed with following errors: ".mysqli_connect_error();
 	die();
 }
 
-require_once $_SERVER['DOCUMENT_ROOT'].'/women-empowerment/config.php';
+// require_once $_SERVER['DOCUMENT_ROOT'].'/women-empowerment/config.php';
+require_once $_SERVER['DOCUMENT_ROOT'].'/aqueous-coast-08816/config.php';
 require_once BASEURL.'helpers/helpers.php';
 
 if(isset($_SESSION['SBUser'])){
