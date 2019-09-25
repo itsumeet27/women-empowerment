3fbab570a78145f7e34737c26b98e5c4c4d57701
Sumeet Sharma <sksksharma0@gmail.com>
2019-09-25 14:15:38 +0530

    Reverting changes

--- config.php
+++ config.php
@@ -1,7 +1,7 @@
 <?php
 
-	define('BASEURL', $_SERVER['DOCUMENT_ROOT'].'/aqueous-coast-08816/');
-	define('CART_COOKIE', 'Sum123Am1902intern');
+	define('BASEURL', $_SERVER['DOCUMENT_ROOT'].'/women-empowerment/');
+	define('CART_COOKIE', 'Sum123Am1996women');
 	define('CART_COOKIE_EXPIRE',time() + (86400 *30));
 	define('TAXRATE', 0.18);
 ?>
