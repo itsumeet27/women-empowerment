66eaa9081445b04346c43cbc499bea6c69b7640a
Sumeet Sharma <sksksharma0@gmail.com>
2019-09-25 13:29:35 +0530

    Changes in config for Heroku App

--- config.php
+++ config.php
@@ -1,6 +1,6 @@
 <?php
 
-	define('BASEURL', $_SERVER['DOCUMENT_ROOT'].'/internship/');
+	define('BASEURL', $_SERVER['DOCUMENT_ROOT'].'/aqueous-coast-08816/');
 	define('CART_COOKIE', 'Sum123Am1902intern');
 	define('CART_COOKIE_EXPIRE',time() + (86400 *30));
 	define('TAXRATE', 0.18);
