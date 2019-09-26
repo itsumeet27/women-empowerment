1f04ff1ec120d4d4c91298144f0e7e04c94a6dad
Sumeet Sharma <sksksharma0@gmail.com>
2019-09-26 23:36:39 +0530

    Adding footer to index page

--- includes/footer.php
+++ includes/footer.php
@@ -1,5 +1,95 @@
-<footer>
-	
+<footer class="page-footer" style="font-family: Roboto;background: #004964">
+	<div class="container pt-5 mb-4 text-center text-md-left">
+      	<div class="row">
+	        <!--First column-->
+	        <div class="col-md-3 col-lg-4 col-xl-3 mb-4">
+	          <h6 class="text-uppercase font-weight-bold">
+	            <strong>Who We Are</strong>
+	          </h6>
+	          <hr class="white accent-2 mb-4 mt-0 d-inline-block mx-auto" style="width: 60px;">
+	          <p>
+	            <a href="about.php">About Us</a>
+	          </p>
+	          <p>
+	            <a href="legislations.php">Legislations</a>
+	          </p>
+	          <p>
+	            <a href="faq.php">FAQ</a>
+	          </p>
+	          <p>
+	            <a href="contact.php">Contact Us</a>
+	          </p>
+	        </div>
+	        <!--/.First column-->
+
+	        <!--Second column-->
+	        <div class="col-md-2 col-lg-2 col-xl-2 mx-auto mb-4">
+	          <h6 class="text-uppercase font-weight-bold">
+	            <strong>STEP Programme</strong>
+	          </h6>
+	          <hr class="white accent-2 mb-4 mt-0 d-inline-block mx-auto" style="width: 60px;">
+	          <p>
+	            <a href="user/guidelines.php">Guidelines</a>
+	          </p>
+	          <p>
+	            <a href="user/training.php">Training Sector</a>
+	          </p>
+	          <p>
+	            <a href="user/ngo.php">NGO(s)</a>
+	          </p>
+	          <p>
+	            <a href="user/login.php">Login</a>
+	          </p>
+	        </div>
+	        <!--/.Second column-->
+
+	        <!--Third column-->
+	        <div class="col-md-3 col-lg-2 col-xl-2 mx-auto mb-4">
+	          <h6 class="text-uppercase font-weight-bold">
+	            <strong>NGO(s)</strong>
+	          </h6>
+	          <hr class="white accent-2 mb-4 mt-0 d-inline-block mx-auto" style="width: 60px;">
+	          <p>
+	            <a href="ngo/criteria.php">Criteria</a>
+	          </p>
+	          <p>
+	            <a href="ngo/funding.php">Funding Norms</a>
+	          </p>
+	          <p>
+	            <a href="ngo/organizations.php">Organizations</a>
+	          </p>
+	          <p>
+	            <a href="ngo/login.php">Login</a>
+	          </p>
+	        </div>
+	        <!--/.Third column-->
+
+	        <!--Fourth column-->
+	        <div class="col-md-4 col-lg-3 col-xl-3">
+	          <h6 class="text-uppercase font-weight-bold">
+	            <strong>Contact</strong>
+	          </h6>
+	          <hr class="white accent-2 mb-4 mt-0 d-inline-block mx-auto" style="width: 60px;">
+	          <p style="text-align: justify;">
+	            <i class="fa fa-home mr-3"></i> 302, B-7, Sector-9, Shanti Nagar, Mira Road (E) - 401107
+	          </p>
+	          <p>
+	            <i class="fa fa-envelope mr-3"></i> sksksharma0@gmail.com
+	          </p>
+	          <p>
+	            <i class="fa fa-phone mr-3"></i> +91 8286864601
+	          </p>                
+	     	</div>
+	        <!--/.Fourth column-->
+
+	    </div>
+	</div>
+
+	<!-- Copyright -->
+	<div class="footer-copyright text-center py-3">&copy; 2019 Copyright:
+	  	<a href="https://www.netmatesolutions.in/women-empowerment"> Women Empowerment</a> developed and maintained by <a href="https://www.lukaenterprises.com">Sumeet Sharma</a>
+	</div>
+	<!-- Copyright -->
 </footer>
 	<!-- SCRIPTS -->
 	<!-- JQuery -->
